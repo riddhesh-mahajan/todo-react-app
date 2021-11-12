982b88542f1b846d965977ea1b52d1d60f552dfa
riddhesh-mahajan <riddheshgp@gmail.com>
2021-11-12 12:23:31 +0530

    feat: save todolist data in local storage

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 import TodoList from './TodoList';
 
+const LOCAL_STORAGE_KEY = 'todoapp.todos'
 
 function App() {
   const [todos, setTodos] = useState([])
   const todoNameRef = useRef()
 
+  useEffect(()=>{
+    const localStorageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (localStorageTodos == null) return
+    
+    setTodos(localStorageTodos)
+  }, [])
+  
+  useEffect(()=>{
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+  
   function addTodo(){
     const name = todoNameRef.current.value;
     if(name == null) return
